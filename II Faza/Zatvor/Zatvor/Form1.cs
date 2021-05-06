4461b135384e5e5dc3f196412984e1a53eb18443
Stefan Aleksic <sssteeefaaan@elfak.rs>
2021-05-06 23:52:53 +0200

    V2.1

---
+++ II Faza/Zatvor/Zatvor/Form1.cs
@@ -0,0 +1,27 @@
+﻿using NHibernate;
+using System;
+using System.Windows.Forms;
+
+namespace Zatvor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                ISession s = DataLayer.GetSession();
+
+                s.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
